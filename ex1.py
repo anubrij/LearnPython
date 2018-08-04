aa19d795526df5fcd9b016aadb5903f7d142339e
Anubrij Chndra <a.chandra@projectobjects.com>
2018-08-04 13:06:32 +0530

    My first commit

---
+++ ex1.py
@@ -0,0 +1,7 @@
+#This program is just to understand the print function of python 3,
+#in python 2 we do not use braces "()" the print can be used like : print "Hi there"
+print("Hello There")
+print("Hi")
+print("My Name Is Anubrij")
+print("This is going to be fun")
+print("Oh! yes the print is working")
---
+++ ex2.py
@@ -0,0 +1,12 @@
+#Lets do some math in python
+print("How many seconds in one year" , 365 * 24 * 60 * 60)
+print ("what is the value of 3 % 2 : " , 3 % 2)
+print("what is the value of 4 % 2 : " , 4 % 2)
+#Lets do some division
+print("3/2 = " , 3/2)
+print("4/2 = " , 4/2)
+#here is intresting division
+print("10/3 = " , 10/3)
+print("10/0.5 = " , 10/0.5)
+#Lets do some compare operators
+print("is 10 is less then 11 , ", 10 < 11)
